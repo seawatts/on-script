f17004510ce52623bd53b28cfde160f026426480
Chris Watts <chris.watts.t@gmail.com>
2024-06-15 11:36:54 -0700

    fix: env

--- apps/nextjs/src/env.ts
+++ apps/nextjs/src/env.ts
@@ -8,36 +8,32 @@ export const env = createEnv({
    * Specify your client-side environment variables schema here.
    * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
    */
-client: {
+  client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
-  
 
-/**
+  /**
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
    */
-experimental__runtimeEnv: {
+  experimental__runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
 
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
-  
-  
 
+  extends: [vercel()],
 
-extends: [vercel()],
-
-  
-  
-/**
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+  runtimeEnv: process.env as any,
+  /**
    * Specify your server-side environment variables schema here.
    * This way you can ensure the app isn't built with invalid env vars.
    */
-server: {
+  server: {
     OPENAI_API_KEY: z.string(),
     POSTGRES_URL: z.string().url().optional(),
   },
-  
+
   shared: {
     NODE_ENV: z
       .enum(["development", "production", "test"])
